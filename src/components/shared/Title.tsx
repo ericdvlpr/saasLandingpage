78ba4b4d0fc02fb8e1b28604598f022e47632d5c
ericdvlpr <ericjaucian@gmail.com>
2025-06-08 08:01:35 +0800

    error fixes

--- src/components/shared/Title.tsx
+++ src/components/shared/Title.tsx
@@ -1,9 +1,11 @@
-interface TitleProps{
-    children: React.ReactNode;
-    className?:string 
+interface TitleProps {
+  children: React.ReactNode;
+  className?: string;
 }
-export const Title = ({children,className=""}: TitleProps) => {
+export const Title = ({ children }: TitleProps) => {
   return (
-    <h1 className="text-heading-1 font-semibold text-2xl sm:text-3xl md:text-4xl" >{children}</h1>
-  )
-}
+    <h1 className="text-heading-1 font-semibold text-2xl sm:text-3xl md:text-4xl">
+      {children}
+    </h1>
+  );
+};
